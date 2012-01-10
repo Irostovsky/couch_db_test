7010d3403d73fa84b7651d82395a0dfc99e7b6f0
Ivan Rostovsky <ir@anahoret.com>
2012-01-10 11:45:56 +0200

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CoachDbTest::Application.config.secret_token = '4806c2396b67e1c71a3cf91b81504e1f70763b9020fb3c82f5ad7159adc7615b1517c881164be0a683c5d5cdc7c0a64e2634c3aa078df6dce154664882e461ad'
