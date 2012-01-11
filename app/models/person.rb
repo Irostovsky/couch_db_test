b595b0a736518d5ceb808f6c33a70f6a4c389c71
Ivan Rostovsky <ir@anahoret.com>
2012-01-11 15:47:26 +0200

    Added tags

--- app/models/person.rb
+++ app/models/person.rb
@@ -2,11 +2,20 @@ class Person < CouchRest::Model::Base
   
   property :name, String
   property :age, Integer
-  property :addresses, [String]
+  property :tags, [String]
   timestamps!
 
-  view_by :name
+  # view_by :name
   
   validates :name, :age, :presence => true
   validates :age, :numericality => {:greater_than => 0}
+
+  def tags
+    read_attribute(:tags).join(', ')
+  end
+  
+  def tags=(str)
+    str = str.split(',').map(&:strip) unless str.is_a? Array
+    write_attribute(:tags, str)
+  end
 end
--- spec/models/person_spec.rb
+++ spec/models/person_spec.rb
@@ -6,6 +6,15 @@ describe Person do
     it { should validate_presence_of(:name) }
     it { should validate_presence_of(:age) }
     it { should validate_numericality_of(:age) }
+  end
+  
+  describe "tags=" do
+    it "should write nil" do
+      Person.new.tags.should be_empty
+    end
     
+    it "should write string with ," do
+      Person.new(:tags => "tag1, tag2")["tags"].should == ["tag1", "tag2"] 
+    end
   end
 end
